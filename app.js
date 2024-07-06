eec0d3c93f20239af99afc47c3b4b5bf1660b045
alifaisal987 <alifaisal987@gmail.com>
2024-07-06 18:27:22 +0500

    assignment 3 done

---
+++ app.js
@@ -0,0 +1,23 @@
+
+ var age=20;
+
+alert("iam" +age + "years old");
+
+ var visitTimes=14;
+alert("You have visited the website"+visitTimes + "Times")
+
+var birthYear=+prompt ("Enter Your birth Year");
+alert(+birthYear);
+
+
+var VisitorName= prompt("Enter Your Name");
+alert('Welcome to our Website \n ' + VisitorName);
+
+var ProductTitle = prompt(' Enter Prodcut ordered')
+alert('You have ordered \n' + ProductTitle);
+
+var quantity= +prompt ('Enter the quantity')
+alert('You ordered \n' +  quantity + 'item')
+
+alert(VisitorName + ' ' + 'ordered' + '' 
++ quantity+'' + ProductTitle + '' +'on XYZ Store'); 
